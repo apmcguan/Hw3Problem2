c01ca3a307e24de729560ccde8d5b6062755aebb
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-11 20:42:55 +0000

    Initial commit

--- main.cpp
+++ main.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
+  int x1, x2, y1, y2;
+  double slope= ((y2-y1)/(x2-x1));
+  x1 = 3;
+  x2 = 8;
+  y1 = 7;
+  y2 = 12;
+  
+  cout<< "The Slope of the line including points (3,7) and (8,12)= "<<slope <<endl;
+  
 }
